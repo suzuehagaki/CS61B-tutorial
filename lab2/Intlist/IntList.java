c4083ca4669631ec38045136ec6f4ca2543a55b2
suzuehagaki <862861415@qq.com>
2023-07-27 16:34:56 +0800

    少了边界值测试

--- lab2/Intlist/IntList.java
+++ lab2/Intlist/IntList.java
@@ -82,6 +82,9 @@ public class IntList {
 
     public static IntList dcatenate(IntList A, IntList B) {
         //TODO:  fill in method
+        if(A == null){
+            return B
+        }
         IntList res;
         res = A.rest;
         while(res.rest != null){
